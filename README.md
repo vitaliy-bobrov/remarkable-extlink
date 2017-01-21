cf50c466ae2c3c67ed0cccfaa34b2c14171eb402
Vitaliy Bobrov <bobrov1989@gmail.com>
2017-01-21 22:35:59 +0100

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # remarkable-extlink
-Adds `target` and `rel` attributes for external links.
+(Remarkable)[https://github.com/jonschlinkert/remarkable] plugin adds `target` and `rel` attributes for external links.
+
+## Installation
+`npm install --save-dev remarkable-extlink`
+
+## Usage
+```js
+const Remarkable = require('remarkable');
+const extLink = require('remarkable-extlink');
+const md = new Remarkable();
+
+md
+  .use(extlink, {
+    host: 'my-host.com'
+  });
+```
+
+## Options
+
+### host {String}
+
+**Required**
+
+You site host name to detect external links.
+
+### target {String}
+
+Target link attribute value, default `_blank`.
+
+### rel {String}
+
+Rel link attribute value, default `nofollow noreferrer noopener`.
---
+++ index.js
@@ -0,0 +1,44 @@
+const url = require('url');
+
+const DEFAUL_OPTIONS = {
+  target: '_blank',
+  rel: 'nofollow noreferrer noopener'
+};
+
+/**
+ * Defines link is internal.
+ * @param host {String} Site hostname.
+ * @param href {Object} Parsed url object.
+ * @return {Boolean}
+ */
+const isInternal = (host, href) => {
+  return href.host === host || (!href.protocol && !href.host && href.pathname);
+};
+
+const remarkableExtLink = (md, options) => {
+  const config = Object.assign({}, DEFAUL_OPTIONS, options);
+
+  // Parse and normalize hostname.
+  config.host = url.parse(config.host).host;
+  // Save original method to invoke.
+  const originalRender = md.renderer.rules.link_open;
+
+  md.renderer.rules.link_open = function(tokens, idx, options, env) {
+    var result;
+
+    // Invoke original method first.
+    result = originalRender.apply(null, arguments);
+
+    regexp = /href="([^"]*)"/;
+
+    let href = url.parse(regexp.exec(result)[1]);
+
+    if (!isInternal(config.host, href)) {
+      result = result.replace('>', ` target="${config.target}" rel="${config.rel}">`);
+    }
+
+    return result;
+  };
+};
+
+module.exports = remarkableExtLink;
